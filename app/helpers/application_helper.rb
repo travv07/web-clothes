7f496f4951bc339e18d6a139105c5d87b8f98e7a
vo van tra <vovantra1198@gmail.com>
2020-04-09 17:46:08 +0700

    sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
     base_title = "Shop clothes"
     page_title.blank? ? base_title : page_title + " | " + base_title
   end
+
+  def flash_class level
+    case level
+      when "notice" then "alert alert-info"
+      when "success" then "alert alert-success"
+      when "error" then "alert alert-danger"
+      when "alert" then "alert alert-warning"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+  devise_for :users, controllers: { registrations: "users/registrations" }
   root "static_pages#home"
 
 end
