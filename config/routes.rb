240f77111eb93e98c52d76cfa7544a74141a2690
vo van tra <vovantra1198@gmail.com>
2020-04-09 18:12:37 +0700

    sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, controllers: { registrations: "users/registrations",
+    sessions: "users/sessions",
+    confirmations: "users/confirmations",
+    passwords: "users/passwords" }
   root "static_pages#home"
 
 end
