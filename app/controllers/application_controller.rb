e13cc9e4bb9b079227104f1c0d064b4cb7930b60
vo van tra <vovantra1198@gmail.com>
2020-04-10 10:47:35 +0700

    update account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,5 +28,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :email, :password, :password_confirmation, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :email, :phone_number])
   end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,6 +10,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     redirect_to root_path
   end
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
